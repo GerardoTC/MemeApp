76a972b76da3da966faf2199538eaa5eaac393f0
Gerardo Tarazona Caceres <gtarazonagb@sofi.org>
2020-04-09 10:26:28 -0500

    cell for meme collection view

---
+++ MeMe App/Cells/MemeCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemeCollectionViewCell.swift
+//  MeMe App
+//
+//  Created by Gerardo Tarazona Caceres on 4/9/20.
+//  Copyright © 2020 udacity. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memedImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func set(image: UIImage) {
+        memedImage.contentMode = .scaleToFill
+        memedImage.image = image
+    }
+}
